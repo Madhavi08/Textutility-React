a8ecd660cb327597fd292b5184dc445c74937b1f
Madhavi Parmar <madhaviparmar341@gmail.com>
2023-01-02 20:25:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const setAlertwithType = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = "#121212"
+      document.body.style.color = 'white'
+      setAlertwithType('Dark Mode Enabled', 'success')
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = 'black'
+      setAlertwithType('Light Mode Enabled', 'success')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          
+          <Routes>
+            <Route path="/" element={<TextForm setAlertwithType={setAlertwithType} heading="Enter the Text to Analyze" mode={mode} />}>
+              
+            </Route>
+            <Route path="/about" element={<About/>}>
+             
+            </Route>
+            
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+    const [text,setText]=useState('')
+    const upClick=()=>{
+        let newText=text.toUpperCase()
+        setText(newText)
+        props.setAlertwithType("converted to uppercase","success")
+    }
+    const lowClick=()=>{
+        let newText=text.toLowerCase()
+        setText(newText)
+        props.setAlertwithType("converted to lowercase","success")
+    }
+    const sentenceClick=()=>{
+        let newText=text.split('. ')
+        newText=newText.map(ele=>{
+            return ele.charAt(0).toUpperCase()+ele.substring(1,ele.length).toLowerCase()      
+        })   
+        setText(newText.join(". "))
+        props.setAlertwithType("converted to sentencecase","success")
+    }
+    const copyClick=()=>{
+        let newText=document.getElementById('mybox')
+        newText.select()
+        navigator.clipboard.writeText(newText.value)
+        props.setAlertwithType("copied to clipboard ","success")
+    }
+    const clearClick=()=>{
+        let newText=''
+        setText(newText)
+        props.setAlertwithType("cleared","success")
+    }
+    const changeClick=(event)=>{
+        setText(event.target.value)
+    }
+    return (
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="mybox" rows="10" value={text} onChange={changeClick} style={{backgroundColor: props.mode ==='light'?'white':'#121212',color:props.mode ==='light'?'black':'white'}}></textarea>
+            </div>
+            <div className="btn btn-primary mx-1" onClick={upClick}>Convert to Uppercase</div>
+            <div className="btn btn-primary mx-1" onClick={lowClick}>Convert to Lowercase</div>
+            <div className="btn btn-primary mx-1" onClick={sentenceClick}>Convert to Sentencecase</div>
+            <div className="btn btn-primary mx-1" onClick={copyClick}>Copy Text</div>
+            <div className="btn btn-primary mx-1" onClick={clearClick}>Clear Text</div>
+        </div>
+            
+        <div className="container my-3">
+            <h1>Your Text summary:</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.08 * text.split(" ").length} minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Enter your text in the above Textbox to preview"}</p>
+        </div>
+        </>
+    )
+}
